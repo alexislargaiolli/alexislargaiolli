d61f6273e871d23416eb45ebd7a7405f8d6782eb
Alexis Largaiolli <alexis.largaiolli@gmail.com>
2017-10-03 11:31:53 +0200

    Change transporter config for nodemailer

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ nodemailer.createTestAccount((err, account) => {
   transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
-    secure: false, // true for 465, false for other ports
+    secure: true, // true for 465, false for other ports
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PWD
